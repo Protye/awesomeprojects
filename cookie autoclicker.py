e649919d5b67f0f3acbab22eb91befbf99f65047
Huy Tran <httran9@ncsu.edu>
2025-07-07 05:18:12 -0400

    functional

--- cookie autoclicker.py
+++ cookie autoclicker.py
@@ -3,9 +3,30 @@ import threading
 from pynput.mouse import Controller, Button
 from pynput.keyboard import Listener, KeyCode
 
-#toggle button
+'''the Listener modules listens for keyboard inputs, while the clicker is running as a separate thread in background.
+when you press the toggle button, the conditional statement in clicker function runs, which does the autoclicking.
 
+'''
 TOGGLE_KEY = KeyCode(char="t")
 
-clicking = False # what thinking did we do to define this variable?
+clicking = False # this is because clicker() is always running in background and we don't want it to activate it's code until we turn it on by pressing our toggle button.
 mouse = Controller()
+
+def clicker():
+    while True:
+        if clicking:
+            mouse.click(Button.left, 1)
+        time.sleep(0.0001) #allows downtime for toggling
+
+def toggle_event(key):
+    if key == TOGGLE_KEY:
+        global clicking #why did we define global?
+        clicking = not clicking
+
+
+click_thread = threading.Thread(target=clicker) #main thread listening for toggle key, while other thread does clicking
+click_thread.start()
+
+with Listener(on_press=toggle_event) as listener:
+    listener.join()
+
