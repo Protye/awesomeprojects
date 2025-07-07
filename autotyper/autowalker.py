0654511db1a14682bb01c9cfcfbb2c220284e4b6
Huy Tran <httran9@ncsu.edu>
2025-07-07 06:39:35 -0400

    working autowalker 4 Roblox

---
+++ autotyper/autowalker.py
@@ -0,0 +1,33 @@
+import keyboard
+import time
+import threading
+from pynput.keyboard import Listener, KeyCode
+
+
+'''once pressed toggled button, continuous press down on the key 'w' until toggled off
+'''
+
+TOGGLE_KEY = KeyCode(char="t")
+
+pressing = False
+
+def toggle_event(key):
+    if key == TOGGLE_KEY:
+        global pressing #global to refer to the global instead of a one-time local
+        pressing = not pressing
+
+
+def walker():
+    while True:
+        if pressing: #why do I need this ifstatement? why not just the while?
+            keyboard.press("w")
+        else:
+            keyboard.release("w")
+        time.sleep(0.01)
+
+click_thread = threading.Thread(target=walker) #main thread listening for toggle key, while other thread does clicking
+click_thread.start()
+
+with Listener(on_press=toggle_event) as listener:
+    listener.join()
+
